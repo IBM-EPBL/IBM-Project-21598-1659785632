7e394c9e82c72951d8921c816d16346fdade8589
shafnaazmi <110673949+shafnaazmi@users.noreply.github.com>
2022-11-21 15:40:01 +0530

    Update app.py

--- Project Development Phase/Sprint 3/Flask/app.py
+++ Project Development Phase/Sprint 3/Flask/app.py
@@ -13,5 +13,21 @@ def my_link():
   print("hello")
   return "done"
 
-
 app.run()
+
+Serving Flask app "__main__" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: off
+INFO:werkzeug: * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+ * Running on http://8af2-35-245-48-50.ngrok.io
+ * Traffic stats available on http://127.0.0.1:4040
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:07] "GET / HTTP/1.1" 200 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:08] "GET /favicon.ico HTTP/1.1" 404 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:15] "GET / HTTP/1.1" 200 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:23] "GET / HTTP/1.1" 200 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:30] "GET / HTTP/1.1" 200 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:38] "GET / HTTP/1.1" 200 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:45] "GET / HTTP/1.1" 200 -
+INFO:werkzeug:127.0.0.1 - - [08/Nov/2022 15:22:53] "GET / HTTP/1.1" 200 -
