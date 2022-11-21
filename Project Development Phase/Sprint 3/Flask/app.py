8fd77aa5cd01020e16b43391ed9f1f25cb06e3a9
shafnaazmi <110673949+shafnaazmi@users.noreply.github.com>
2022-11-21 15:35:35 +0530

    Update app.py

--- Project Development Phase/Sprint 3/Flask/app.py
+++ Project Development Phase/Sprint 3/Flask/app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask import Flask, render_template
 
+from flask_ngrok import run_with_ngrok
+app = Flask(__name__)
+run_with_ngrok(app)   
+@app.route(r'/')
+def index():
+  return render_template('index.html')
+
+@app.route('/my-link/')
+def my_link():
+  print("hello")
+  return "done"
+
+
+app.run()
